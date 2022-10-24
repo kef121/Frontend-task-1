5cfc82484ec760d43c9e87f7aaa2c43279973bea
favour <favourokeke958@gmail.com>
2022-10-24 11:57:24 +0100

    I just added the button links and socials

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import Links from "./components/Links";
 import Profile from "./components/Profile";
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
         <Profile />
+        <Links />
     </div>
   );
 }
---
+++ frontend1/src/components/Links.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Slack from '../assets/slack.png'
+import Github from '../assets/Github.png'
+import "./styles.css";
+
+const Links = () => {
+  return (
+    <section id="links">
+        <button id="twitter__link"><a href='https://twitter.com/keffy21'>Twitter Link</a></button>
+        <button id="btn__zuri"><a href='https://training.zuri.team/'>Zuri Team</a></button>
+        <button id="books"><a href='https://books.zuri.team'>Zuri Books</a></button>
+        <button id="books__python"><a href='https://books.zuri.team/'>Python Books</a></button>
+        <button id="pitch"><a href='https://background.zuri.team'>Background Check for Coders</a></button>
+        <button id="book__design"><a href='https://books.zuri.team/design-rules'>Design Books</a></button>
+        <div id='socials'>
+            <img id="slack-icon" alt="slack" src={Slack}></img>
+            <a href='https://github.com/kef121'><img id="github-icon" alt="github" src={Github}></img></a>
+        </div>
+    </section>
+  )
+}
+
+export default Links
